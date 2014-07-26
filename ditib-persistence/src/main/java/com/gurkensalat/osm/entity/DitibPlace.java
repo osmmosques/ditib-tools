065d9c0d8fc85b695a19b35db0de5dc490f4d348
Hakan Tandogan <hakan@gurkensalat.com>
2014-07-26 13:11:21 +0200

    sonar grooming

--- ditib-persistence/src/main/java/com/gurkensalat/osm/entity/DitibPlace.java
+++ ditib-persistence/src/main/java/com/gurkensalat/osm/entity/DitibPlace.java
@@ -2,15 +2,8 @@ package com.gurkensalat.osm.entity;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
