aefe15697b54e25ec42ca0cba30bc2400e5af76c
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-20 19:25:47 +0200

    Rename submodule to better reflect its purpose

--- combined-persistence/src/main/java/com/gurkensalat/osm/repository/RepositoryComponentScanMarker.java
+++
@@ -1,8 +0,0 @@
-package com.gurkensalat.osm.repository;
-
-/**
- * Empty marker interface to make this package visible to EnableJpaRepositories.
- */
-public interface RepositoryComponentScanMarker
-{
-}
