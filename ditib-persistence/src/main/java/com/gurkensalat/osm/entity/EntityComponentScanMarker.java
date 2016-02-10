e47c14469455b4aaf4834f635e53b7199c37298b
Hakan Tandogan <hakan@tandogan.com>
2016-02-10 17:11:36 +0100

    Fix Javadoc breakage

--- ditib-persistence/src/main/java/com/gurkensalat/osm/entity/EntityComponentScanMarker.java
+++ ditib-persistence/src/main/java/com/gurkensalat/osm/entity/EntityComponentScanMarker.java
@@ -3,8 +3,7 @@ package com.gurkensalat.osm.entity;
 /**
  * Empty marker interface to make this package visible to EnableJpaRepositories.
  *
- * @see http://stackoverflow.com/questions/23366226/spring-boot-w-jpa-move-entity-to-different-package
- * @see
+ * @see <a href="http://stackoverflow.com/questions/23366226/spring-boot-w-jpa-move-entity-to-different-package">StackOverflow explanation</a>
  */
 public interface EntityComponentScanMarker
 {
