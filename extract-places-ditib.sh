4f5c8669b0754cbaeaec16c26cbde7a76ec9b211
Hakan Tandogan <hakan@tandogan.com>
2016-03-10 20:06:32 +0100

    Leave not geocoded places on Zero Island

--- extract-places-ditib.sh
+++ extract-places-ditib.sh
@@ -111,9 +111,9 @@ DB_DIR=${STORAGE}/database/${MONTH}/${DAY}
 
 mkdir -p ${DB_DIR}
 
-mysql -uroot -p$(cat ${HOME}/.my.pass) ${db} \
-    -e "update ditib_places set lat = 46.42, lon = 6.55 where lat = 0 and lon = 0;" \
-    > ${DB_DIR}/${db}-mosques-in-lac-leman.sql
+# mysql -uroot -p$(cat ${HOME}/.my.pass) ${db} \
+#     -e "update ditib_places set lat = 46.42, lon = 6.55 where lat = 0 and lon = 0;" \
+#     > ${DB_DIR}/${db}-mosques-in-lac-leman.sql
 
 mysqldump -uroot -p$(cat ${HOME}/.my.pass) --skip-extended-insert ${db} \
     > ${DB_DIR}/${db}-dump.sql
